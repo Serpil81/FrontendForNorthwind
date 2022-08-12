868978c8bf53ab4e01ad44044a9320bb0ac57f7b
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2022-08-12 21:21:37 +0300

    Project Week 19

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -17,7 +19,12 @@ export class ProductComponent implements OnInit {
 
   
 
-  constructor(private productService:ProductService, private activatedRoute:ActivatedRoute) { }
+  constructor(
+    private productService:ProductService, 
+    private activatedRoute:ActivatedRoute,
+    private toastrService:ToastrService,
+    private cartService: CartService)
+     { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params=>{
@@ -45,7 +52,8 @@ export class ProductComponent implements OnInit {
       })
        }
        addToCart(product:Product){
-console.log(product);
+this.toastrService.success("Added to Cart", product.productName)
+this.cartService.addToCart(product);
        }
 
 }
